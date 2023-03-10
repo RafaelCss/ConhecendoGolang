e9f60cecf608d852b4aef5d677dccd46f3907c50
devspacerj <rf2014souza@gmail.com>
2023-03-10 13:50:01 -0300

    aprendendo Go

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	contaCorrenteRafael := ContaCorrente{"Rafael", 599, 5998, 150.5}
 	fmt.Println(contaCorrenteRafael.Sacar(50.10))
 	fmt.Println(contaCorrenteRafael.saldo)
+	fmt.Print(contaCorrenteRafael.Depositar(250))
+	fmt.Println(contaCorrenteRafael.saldo)
 }
 
 
@@ -24,4 +26,9 @@ func (conta * ContaCorrente) Sacar (valorDoSaque float64) string{
 	}
 	conta.saldo -= valorDoSaque
 	return "Valor Sacado foi de ..."
+}
+
+func (conta * ContaCorrente) Depositar (valorDoDeposito float64) string{
+	conta.saldo += valorDoDeposito
+	return "Valor Depositado foi de ..."
 }
